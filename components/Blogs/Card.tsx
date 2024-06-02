49c003813762447f2ac38905ac16327ee1617326
Mohtashim Ali <mohtashima85@gmail.com>
2024-06-02 22:21:24 +0500

    Create Card.tsx

---
+++ components/Blogs/Card.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { IconArrowUpRight } from '../icons';
+import { Blog } from '@/interface/blog';
+
+const Card: React.FC<Blog> = ({ id, title, description, category }) => {
+  return (
+    <div className='bg-white shadow-lg p-6 space-y-8 basis-full sm:basis-1/2 md:basis-1/4 md:min-w-[200px]'>
+      <Image src={`/blogs/${id}.png`} width={336} height={240} alt={title} />
+      <div className='space-y-3'>
+        <p className='text-sm text-blue-950'>{category}</p>
+        <Link href={`/${id}`} className=''>
+          <h2 className='font-semibold text-2xl flex justify-between gap-1 items-center'>
+            <span className='text-nowrap w-fit truncate'>{title}</span>
+            <IconArrowUpRight />
+          </h2>
+        </Link>
+        <p className='text-gray-500'>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
