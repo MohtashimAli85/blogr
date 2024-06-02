141969666f4c40476693bfd37251278c8268faba
Mohtashim Ali <mohtashima85@gmail.com>
2024-06-02 22:37:47 +0500

    Update index.tsx

--- components/Blogs/index.tsx
+++ components/Blogs/index.tsx
@@ -4,7 +4,7 @@ import Card from './Card';
 const Blogs = async () => {
   const blogs = await getBlogs();
   return (
-    <div className='flex flex-wrap p-4 gap-2 sm:gap-3 md:gap-4 max-w-6xl justify-center mx-auto -mt-20'>
+    <div className='flex flex-wrap p-4 gap-2 sm:gap-3 md:gap-4 max-w-7xl justify-center mx-auto -mt-20'>
       {blogs.map((item, i) => (
         <Card key={item.id} {...item} />
       ))}
